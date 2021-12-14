0df39686d04fd6b3a12c9b3d8e89608353f2062c
Connor <gamester440@gmail.com>
2021-12-13 21:17:09 -0700

    fixed role config

--- src/chat/types/reaction.types.ts
+++ src/chat/types/reaction.types.ts
@@ -1,10 +1,15 @@
 const UYA_REACTIONS: { [emoji: string]: string } = {
-  '876271633592107068': '876549327575724102',
+  // Flux Icon - UYA Players
+  '876271513102323752': '807713673132113951',
+  // Sheepinator Icon - UYA Casuals
+  '876271858947870750': '909561632995282994',
 };
 
 const DL_REACTIONS: { [emoji: string]: string } = {
-  // Rank 1 : DL Contestant
-  '873718537590542377': '876515332968288286',
+  // Fusion Icon - DL Players
+  '873719220305817660': '807713584360587264',
+  // Arbiter Icon - DL Casuals
+  '873719153964486686': '883145123322527845',
 };
 
 export const ROLE_REACTIONS = {
