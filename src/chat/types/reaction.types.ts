f07f0d37117e76e81648aa112e8d019e27fb3fc2
Connor <gamester440@gmail.com>
2021-12-15 18:04:39 -0700

    updated emojis for reactions

--- src/chat/types/reaction.types.ts
+++ src/chat/types/reaction.types.ts
@@ -1,13 +1,13 @@
 const UYA_REACTIONS: { [emoji: string]: string } = {
-  // Flux Icon - UYA Players
-  '876271513102323752': '807713673132113951',
+  // Nefarious - UYA Players
+  '752680088125964418': '807713673132113951',
   // Sheepinator Icon - UYA Casuals
   '876271858947870750': '909561632995282994',
 };
 
 const DL_REACTIONS: { [emoji: string]: string } = {
-  // Fusion Icon - DL Players
-  '873719220305817660': '807713584360587264',
+  // Vox - DL Players
+  '752472561891409931': '807713584360587264',
   // Arbiter Icon - DL Casuals
   '873719153964486686': '883145123322527845',
 };
