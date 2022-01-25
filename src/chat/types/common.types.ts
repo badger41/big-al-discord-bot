bc44771fa08426fb849e55382c5671eb15f117a7
John Janecek <janecektyler@gmail.com>
2022-01-24 19:12:43 -0700

    Update common.types.ts

--- src/chat/types/common.types.ts
+++ src/chat/types/common.types.ts
@@ -1,6 +1,6 @@
 export const SnarkyRemarks: string[] = [
   'I can do this in my sleep...',
-  "time to back to my Quark vid-comic!",
+  "time to get back to my Qwark vid-comic!",
   'aaaaaaand done!',
   "_you're welcome..._",
   'pfft, amateur. Give me a challenge next time.',
