b16fead5c0e6bfb597b9694a3999067239b186d0
Daniel Gerendasy <danielgerendasy@gmail.com>
2022-02-09 08:32:04 -0800

    Stop querying when discord rate limit is hit

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,13 @@ dotenv.config();
  */
 const client = new Discord.Client();
 client.login(process.env.BIG_AL_BOT_TOKEN);
+client.options.retryLimit = 0;
+
+let timeoutDate = 0;
+client.on('rateLimit', (info) => {
+  console.log(`Rate limit hit ${info.timeDifference ? info.timeDifference : info.timeout ? info.timeout: 'Unknown timeout '} (${info.timeDifference}, ${info.timeout}, ${info.method}, ${info.path}, ${info.route}, ${info.limit})`)
+  timeoutDate = Date.now() + (info.timeDifference ? info.timeDifference : info.timeout ? info.timeout : 10000);
+})
 
 /**
  * Initialize the DL online players script.
@@ -25,13 +32,19 @@ client.on('ready', () => {
 
   if (process.env.DL_PLAYERS_ONLINE_ENABLED === 'true') {
     client.setInterval(
-      () => checkOnlineDLPlayers(client),
+      () => {
+        if (Date.now() > timeoutDate)
+          checkOnlineDLPlayers(client)
+      },
       Number(process.env.DL_PLAYERS_ONLINE_INTERVAL) || 60000
     );
   }
   if (process.env.UYA_PLAYERS_ONLINE_ENABLED === 'true') {
     client.setInterval(
-      () => checkOnlineUYAPlayers(client),
+      () => {
+        if (Date.now() > timeoutDate)
+          checkOnlineUYAPlayers(client)
+      },
       Number(process.env.UYA_PLAYERS_ONLINE_INTERVAL) || 60000
     );
   }
